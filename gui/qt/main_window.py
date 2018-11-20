06ff85029ba996c5f20a9927191a2ec97d72c2ad
rc125 <rc125@pm.me>
2018-11-20 02:03:17 -0800

    Alpha 5

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -587,7 +587,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
 
     def show_about(self):
         QMessageBox.about(self, "Electrum-SMART",
-            _("Version")+" %s" % (self.wallet.electrum_version) + " Alpha 4 \n\n" +
+            _("Version")+" %s" % (self.wallet.electrum_version) + " Alpha 5 \n\n" +
                 _("Electrum's focus is speed, with low resource usage and simplifying SmartCash. You do not need to perform regular backups, because your wallet can be recovered from a secret phrase that you can memorize or write on paper. Startup times are instant because it operates in conjunction with high-performance servers that handle the most complicated parts of the SmartCash system."  + "\n\n" +
                 _("Uses icons from the Icons8 icon pack (icons8.com).")))
 
